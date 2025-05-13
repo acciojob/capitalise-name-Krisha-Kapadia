fe2e2790ee2ca0b1c77d512affb5b415749aae25
Krisha-Kapadia <130428693+Krisha-Kapadia@users.noreply.github.com>
2025-05-13 13:24:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+function convertToUpper() {
+  const input = document.getElementById("fname").value;
+  const upperCaseName = input.toUpperCase();
+  alert("Uppercase: " + upperCaseName);
+  document.getElementById("result").textContent = "Uppercase: " + upperCaseName;
+}
